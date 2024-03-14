e4cdec2a622f2d35f53daa28064765e30ce28a94
Josué Salazaku <josuesalazaku@gmail.com>
2024-03-14 09:59:47 +0100

    Create lading pages and landing route

--- Sounds-swap/routes/web.php
+++ Sounds-swap/routes/web.php
@@ -14,6 +14,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/Landing', function () {
+    return Inertia::render(('Landing'));
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
