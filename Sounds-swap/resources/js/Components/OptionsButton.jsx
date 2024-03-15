5793cab874bb8e7e1d2c559c9b0c8ea04d5a06c0
Josué Salazaku <josuesalazaku@gmail.com>
2024-03-15 10:50:16 +0100

    Remove 2 options Button

--- Sounds-swap/resources/js/Components/OptionsButton.jsx
+++ Sounds-swap/resources/js/Components/OptionsButton.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function OptionsButton() {
     const buttonData = [
-        { id: 1, label: 'Lighting', link: '/link1' },
-        { id: 2, label: 'Mixing Board', link: '/link2' },
-        { id: 3, label: 'Microphones', link: '/link3' },
-        { id: 4, label: 'Monitors', link: '/link4' },
-        { id: 5, label: 'Sound Systems', link: '/link5' },
-        { id: 6, label: 'Products', link: '/link6' },
-        { id: 7, label: 'Button 5', link: '/link7' },
-        { id: 8, label: 'Button 6', link: '/link8' },
+        { id: 1, label: "Lighting", link: "/link1" },
+        { id: 2, label: "Mixing Board", link: "/link2" },
+        { id: 3, label: "Microphones", link: "/link3" },
+        { id: 4, label: "Monitors", link: "/link4" },
+        { id: 5, label: "Sound Systems", link: "/link5" },
+        { id: 6, label: "Products", link: "/link6" },
+
         // Add the links to all the options pages and changes the button names/ labels
     ];
 
     return (
         <div>
-            <ul className='flex space-x-10'>
-                {buttonData.map(button => (
+            <ul className="flex space-x-10">
+                {buttonData.map((button) => (
                     <li key={button.id}>
                         <a href={button.link}>
-                            <button className='bg-secondary py-1 px-6 text-primary rounded-lg font-semibold active:text-white active:bg-primary hover:bg-primary hover:text-white'>
+                            <button className="bg-secondary py-1 px-6 text-primary rounded-lg font-semibold active:text-white active:bg-primary hover:bg-primary hover:text-white">
                                 {button.label}
                             </button>
                         </a>
@@ -30,5 +29,4 @@ function OptionsButton() {
     );
 }
 
-
-export default OptionsButton
+export default OptionsButton;
