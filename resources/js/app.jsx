6340b6162c868868cbae0717b1b61d9abd1ecfb9
Josué Salazaku <josuesalazaku@gmail.com>
2024-03-18 16:51:04 +0100

    Re-add the missing links

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -9,37 +9,39 @@ import Pricing from "./Pages/Pricing";
 import Community from "./Pages/Community";
 import AboutUs from "./Pages/AboutUs";
 
-import OptionsButtons from "./Components/OptionsButtons"; // Import OptionsButtons component
-
-import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react'; 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createRoot } from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
-  resolve: name => {
-    const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
-    return pages[`./Pages/${name}.jsx`]
-  },
-  setup({ el, App, props }) {
-    const AppRoutes = () => (
-      <Routes>
-        <Route exact path="/" element={<App {...props} />} />
-        <Route path="/Products" element={<Products />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/SignIn" element={<SignIn />} />
-        <Route path="/SignUp" element={<SignUp />} />
-      </Routes>
-    );
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
+        return pages[`./Pages/${name}.jsx`];
+    },
+    setup({ el, App, props }) {
+        const AppRoutes = () => (
+            <Routes>
+                <Route exact path="/" element={<App {...props} />} />
+                <Route path="/AboutUs" element={<AboutUs />} />
+                <Route path="/Products" element={<Products />} />
+                <Route path="/Pricing" element={<Pricing />} />
+                <Route path="/Community" element={<Community />} />
+                <Route path="/SignUp" element={<SignUp />} />
+                <Route path="/Contact" element={<Contact />} />
+                <Route path="/SignIn" element={<SignIn />} />
+                <Route path="/SignUp" element={<SignUp />} />
+            </Routes>
+        );
 
-    createRoot(el).render(
-      <Router>
-        <>
-          <Header />
-          <AppRoutes /> 
-        </>
-      </Router>
-    );
-  },
+        createRoot(el).render(
+            <Router>
+                <>
+                    <Header />
+                    <AppRoutes />
+                </>
+            </Router>
+        );
+    },
 });
