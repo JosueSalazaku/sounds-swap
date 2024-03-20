3c517137c44c197f5ab980e266c4cd261a53fd3d
Josué Salazaku <josuesalazaku@gmail.com>
2024-03-20 12:01:52 +0100

    Update data url to display Lights pictures

--- resources/js/DummyData/LightsDummyData.js
+++ resources/js/DummyData/LightsDummyData.js
@@ -33,7 +33,7 @@ const LightsDummyData = [
         description:
             "Create a vibrant atmosphere with this UV black light. Ideal for glow parties, clubs, and special events.",
         price: 59.99,
-        image: "https://m.media-amazon.com/images/I/71cX5Dzid5S._AC_SL1500_.jpg",
+        image: "https://www.gosupps.com/media/catalog/product/7/1/71dBxtOW-WL.jpg",
     },
     {
         id: 5,
@@ -42,7 +42,7 @@ const LightsDummyData = [
         description:
             "Enhance your stage presence with this laser stage light. Create mesmerizing patterns and effects with ease.",
         price: 139.99,
-        image: "https://m.media-amazon.com/images/I/61NE8Jpxe6L._AC_SL1500_.jpg",
+        image: "https://www.syntronics.co.za/cdn/shop/products/ministagelightprojector_1200x1200.jpg?v=1612909542",
     },
     {
         id: 6,
@@ -51,7 +51,7 @@ const LightsDummyData = [
         description:
             "Illuminate large areas with this powerful flood light. Perfect for outdoor events, festivals, and architectural lighting.",
         price: 199.99,
-        image: "https://m.media-amazon.com/images/I/61vtMrcRiyL._AC_SL1500_.jpg",
+        image: "https://img.fruugo.com/product/3/42/348899423_max.jpg",
     },
     {
         id: 7,
@@ -60,16 +60,16 @@ const LightsDummyData = [
         description:
             "Add color and ambiance to any space with these wireless LED uplights. Control brightness and colors wirelessly.",
         price: 89.99,
-        image: "https://m.media-amazon.com/images/I/61zlkq5ngpL._AC_SL1500_.jpg",
+        image: "https://www.fabulousevents.com/wp-content/uploads/2015/08/black-uplight.jpg",
     },
     {
         id: 8,
         category: "Lighting",
-        name: "Effect Light",
+        name: "Effect Light Panels",
         description:
             "Create unique visual effects with this versatile effect light. Ideal for DJs, theaters, and special events.",
         price: 109.99,
-        image: "https://m.media-amazon.com/images/I/81YgIE5WroL._AC_SL1500_.jpg",
+        image: "https://m.media-amazon.com/images/I/51w15vGFTnL._AC_UF894,1000_QL80_.jpg",
     },
     {
         id: 9,
@@ -78,7 +78,7 @@ const LightsDummyData = [
         description:
             "Bring the disco vibe to your party with this classic disco ball. Reflective mirrors create dazzling light patterns.",
         price: 49.99,
-        image: "https://m.media-amazon.com/images/I/71dxOCiDwxL._AC_SL1500_.jpg",
+        image: "https://media-www.canadiantire.ca/product/seasonal-gardening/party-city-seasonal/party-city-micro-season-decor/8551297/10-new-year-s-eve-silver-disco-ball-b5045f3b-8d38-4a83-8fe7-a60820491104-jpgrendition.jpg?imdensity=1&imwidth=640&impolicy=mZoom",
     },
     {
         id: 10,
@@ -87,81 +87,80 @@ const LightsDummyData = [
         description:
             "Create stunning visual displays with this pixel LED panel. Each pixel can be individually controlled for endless possibilities.",
         price: 399.99,
-        image: "https://m.media-amazon.com/images/I/61kSGHVtd1S._AC_SL1500_.jpg",
+        image: "https://m.media-amazon.com/images/I/61jvjNlnIdL._AC_UF894,1000_QL80_.jpg",
     },
     // Mixing Board
     {
         id: 11,
-        category: "Mixing Board",
-        name: "Digital Mixing Console",
+        category: "Lighting",
+        name: "Strobe Light",
         description:
-            "Take your audio mixing to the next level with this digital mixing console. Intuitive interface and advanced features.",
-        price: 899.99,
-        image: "https://m.media-amazon.com/images/I/81n3w72gF3L._AC_SL1500_.jpg",
+            "Create dynamic visual effects with this powerful strobe light. Ideal for DJs, clubs, and parties.",
+        price: 129.99,
+        image: "https://m.media-amazon.com/images/I/613qjlaziJL._AC_UF894,1000_QL80_.jpg",
     },
     {
         id: 12,
-        category: "Mixing Board",
-        name: "Analog Mixing Desk",
+        category: "Lighting",
+        name: "Fog Machine",
         description:
-            "Experience the warmth of analog sound with this classic mixing desk. Perfect for vintage recordings and live performances.",
-        price: 599.99,
-        image: "https://m.media-amazon.com/images/I/71bIkcpGcWL._AC_SL1500_.jpg",
+            "Enhance your lighting effects with this professional-grade fog machine. Perfect for creating atmosphere at events and performances.",
+        price: 199.99,
+        image: "https://thumbs.static-thomann.de/thumb/padthumb600x600/pics/bdb/_40/403498/11733600_800.jpg",
     },
     {
         id: 13,
-        category: "Mixing Board",
-        name: "Portable DJ Controller",
+        category: "Lighting",
+        name: "LED Effect Light",
         description:
-            "Take control of your mixes on the go with this portable DJ controller. Compact size and versatile functionality.",
-        price: 299.99,
-        image: "https://m.media-amazon.com/images/I/81LR6pi2ZxL._AC_SL1500_.jpg",
+            "Add excitement to your events with this LED effect light. Features various patterns and colors for stunning visuals.",
+        price: 79.99,
+        image: "https://d1aeri3ty3izns.cloudfront.net/media/27/279043/1200/preview.jpg",
     },
     {
         id: 14,
-        category: "Mixing Board",
-        name: "Live Sound Mixer",
+        category: "Lighting",
+        name: "Laser Light Show",
         description:
-            "Achieve professional live sound with this versatile live sound mixer. Ideal for bands, theaters, and houses of worship.",
-        price: 799.99,
-        image: "https://m.media-amazon.com/images/I/71zNcT3UVGL._AC_SL1500_.jpg",
+            "Create mesmerizing light shows with this versatile laser light. Perfect for concerts, clubs, and special events.",
+        price: 299.99,
+        image: "https://www.proxdirect.com/images/originals/X-RAW10-01.jpg",
     },
     {
         id: 15,
-        category: "Mixing Board",
-        name: "USB Audio Interface",
+        category: "Lighting",
+        name: "Outdoor LED Floodlight",
         description:
-            "Connect your instruments and microphones to your computer with this USB audio interface. High-quality sound and low latency.",
-        price: 199.99,
-        image: "https://m.media-amazon.com/images/I/81yKpTYyoPL._AC_SL1500_.jpg",
+            "Illuminate outdoor spaces with this powerful LED floodlight. Weatherproof and energy-efficient design.",
+        price: 149.99,
+        image: "https://onforuleds.com/cdn/shop/files/ON-D100-BD24-DW-US-HSA_100W_D_-1_28757761-84f7-4423-a705-d43ec9b79f32_1024x1024.jpg?v=1692700291",
     },
-    // Microphones
     {
         id: 16,
-        category: "Microphones",
-        name: "Wireless Handheld Microphone",
+        category: "Lighting",
+        name: "DMX Controller",
         description:
-            "Experience freedom of movement with this wireless handheld microphone. Ideal for presentations, speeches, and performances.",
-        price: 129.99,
-        image: "https://m.media-amazon.com/images/I/71CS+ajhTVL._AC_SL1500_.jpg",
+            "Take full control of your lighting setup with this DMX controller. Supports multiple channels and presets.",
+        price: 349.99,
+        image: "https://static.bax-shop.es/image/product/808041/3001130/217b9017/1627459235ADJ_Link_Controller_Photos_30.jpg",
     },
     {
         id: 17,
-        category: "Microphones",
-        name: "Shotgun Microphone",
+        category: "Lighting",
+        name: "Wireless LED Par Light",
         description:
-            "Capture clear audio from a distance with this shotgun microphone. Ideal for video production, interviews, and field recording.",
-        price: 179.99,
-        image: "https://m.media-amazon.com/images/I/81dSH19H5SL._AC_SL1500_.jpg",
+            "Achieve wireless lighting control with this LED par light. Compact and easy to set up for versatile applications.",
+        price: 89.99,
+        image: "https://d.yixunpan.top/mjledlighting.com/wp-content/uploads/2020/04/1588129715-2j.jpg",
     },
     {
         id: 18,
-        category: "Microphones",
-        name: "Lavalier Microphone Set",
+        category: "Lighting",
+        name: "UV LED Bar",
         description:
-            "Record hands-free audio with this lavalier microphone set. Includes transmitter, receiver, and clip-on microphone.",
-        price: 89.99,
-        image: "https://m.media-amazon.com/images/I/61wkBsTZFIL._AC_SL1500_.jpg",
+            "Add an ultraviolet glow to your events with this UV LED bar. Great for blacklight parties and stage performances.",
+        price: 59.99,
+        image: "https://static.bax-shop.es/image/product/136764/267934/7081a78e/american_dj_uv_led_bar_20_blacklight_1_2.jpg",
     },
     {
         id: 19,
@@ -170,7 +169,7 @@ const LightsDummyData = [
         description:
             "Project custom images and patterns with this versatile gobo projector. Ideal for branding, events, and theatrical productions.",
         price: 349.99,
-        image: "https://m.media-amazon.com/images/I/71d5JbUMrVL._AC_SL1500_.jpg",
+        image: "https://www.omega.be/images/ashx/adj-gobo-projector-ir-bin1.jpeg?s_id=ADJG46E681&imgfield=s_imagebin1&imgwidth=1200&imgheight=1200",
     },
     {
         id: 20,
@@ -179,7 +178,7 @@ const LightsDummyData = [
         description:
             "Decorate and illuminate any space with these colorful RGB strip lights. Perfect for parties, home decor, and stage backdrops.",
         price: 29.99,
-        image: "https://m.media-amazon.com/images/I/61jHh7q7KRL._AC_SL1000_.jpg",
+        image: "https://m.media-amazon.com/images/I/81XbnuN2okL.jpg",
     },
     // Add more lights, mixing boards, and microphones here...
 ];
