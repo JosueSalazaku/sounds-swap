cd9a581e6488c2f59f6dd11e4692f15cf665b99d
RebeccaVanLitsenborg <rebeccavanlitsenborg@gmail.com>
2024-03-20 11:48:51 +0100

    Edit About Us Content

--- resources/js/Components/AboutUsContent.jsx
+++ resources/js/Components/AboutUsContent.jsx
@@ -20,6 +20,9 @@ const AboutUsContent = () => {
                     <h3 className="font-extrabold text-primary mt-4 mb-2 font-sans text-xl text-center">Trusted Sellers:</h3>
                     <p className="mx-auto p-5">When you shop with Sound Swap, you can shop with confidence. Our sellers are vetted for reliability and 
                     authenticity, so you can trust that you're getting exactly what you pay for.</p>
+                    <h3 className="font-extrabold text-primary mt-4 mb-2 font-sans text-xl text-center">Curated Selection:</h3>
+                    <p className="mx-auto p-5">We understand that quality matters. That's why we handpick every item listed on our platform to ensure 
+                    that only the best audio equipment makes its way to your doorstep.</p>
                     <h3 className="font-extrabold text-primary mt-4 mb-2 font-sans text-xl text-center">Secure Transactions:</h3>
                     <p className="mx-auto p-5">Your security is our top priority. With secure payment options and encrypted transactions, you can rest 
                     easy knowing that your personal information is safe with us.</p>
