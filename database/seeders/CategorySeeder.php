13bfcd101526b33b195fb182ef8db033ac0bcfe2
Josué Salazaku <josuesalazaku@gmail.com>
2024-03-27 14:44:10 +0100

    Add category_id to prodcuts for relations

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -10,42 +10,42 @@ class CategorySeeder extends Seeder
 {
     public function run()
     {
-        Category::create([
+        category::create([
             'name' => 'Lighting',
             'slug' => 'lighting',
             'description' => 'Category for lighting products',
             'order' => 1, 
         ]);
 
-        Category::create([
+        category::create([
             'name' => 'Mixing Board',
             'slug' => 'mixing-board',
             'description' => 'Category for mixing boards',
             'order' => 1, 
         ]);
 
-        Category::create([
+        category::create([
             'name' => 'Microphones',
             'slug' => 'microphones',
             'description' => 'Category for microphones.',
             'order' => 1, 
         ]);
 
-        Category::create([
+        category::create([
             'name' => 'Monitors',
             'slug' => 'monitors',
             'description' => 'Category for monitors.',
             'order' => 1, 
         ]);
 
-        Category::create([
+        category::create([
             'name' => 'Sound Systems',
             'slug' => 'sound-systems',
             'description' => 'Category for sound systems.',
             'order' => 1, 
         ]);
 
-        Category::create([
+        category::create([
             'name' => 'Cord Cables',
             'slug' => 'cord-cables',
             'description' => 'Category for cord cables.',
