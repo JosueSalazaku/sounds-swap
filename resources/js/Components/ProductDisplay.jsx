9b756956a1a5561bf34d5c051e1e9d39c3fa9665
Josué Salazaku <josuesalazaku@gmail.com>
2024-04-03 11:36:01 +0200

    Display pictures

--- resources/js/Components/ProductDisplay.jsx
+++ resources/js/Components/ProductDisplay.jsx
@@ -9,7 +9,8 @@ const ProductDisplay = () => {
             <h1>Products</h1>
             <ul>
                 {products.map((product) => (
-                    <li key={product.id}>
+                  <li key={product.id}>
+                    <img className='h-20' src={product.imgURL}  />
                         <h2>{product.name}</h2>
                         <p>{product.description}</p>
                         <p>€ {product.price}</p>
