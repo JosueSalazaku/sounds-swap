8cfa03c86bbfb5cd362929224cf74463bd019d91
RebeccaVanLitsenborg <rebeccavanlitsenborg@gmail.com>
2024-04-05 11:02:59 +0200

    Update Vite config file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input:  ['resources/js/app.jsx', 'resources/js/Pages/Welcome.jsx', 'resources/js/Pages/AboutUs.jsx', 'resources/js/Pages/Products.jsx'],
+            input:  ['resources/js/app.jsx', 'resources/js/Pages/Welcome.jsx', 'resources/js/Pages/AboutUs.jsx', 'resources/js/Pages/Products.jsx', 'resources/js/Pages/Contact.jsx', 'resources/js/Pages/Checkout.jsx'],
             refresh: true,
         }),
         react(),
